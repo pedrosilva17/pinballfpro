9ee3ed80e18df3c5cffd434806b8ecdfad006a83
pedrosilva17 <75742355+pedrosilva17@users.noreply.github.com>
2020-12-15 12:33:42 +0000

    Add files via upload

--- PYnball.py
+++ PYnball.py
@@ -5,17 +5,32 @@ Created on Sun Dec 13 13:49:25 2020
 @author: Pedro Silva
 """
 
-import pygame 
-pygame.init()
+import pygame
+import sys
+import random
+import math
+import os
+import getopt
+from socket import *
+from pygame.locals import *
 
-window = pygame.display.set_mode((500, 600))
-
-pygame.display.set_caption("PYnball")
+def load_png(name):
+    fullname = os.path.join('C:/Users/Pedro Silva/Documents/FEUP/FPRO/Projeto', name)
+    try:
+        image = pygame.image.load(fullname)
+        if image.get_alpha is None:
+            image = image.convert()
+        else:
+            image = image.convert_alpha()
+    except pygame.error as message:
+        print('Cannot load image: ' + fullname)
+        raise SystemExit(message)
+    return image, image.get_rect()
 
 def redrawWindow():
     window.fill((60,60,60))
-    pinBall.draw(window)
     pygame.display.update()
+
     
 x = 50
 y = 50
@@ -23,26 +38,42 @@ width = 30
 height = 50
 vel = 5
 
-class ball(object):
-    def __init__(self, x, y, radius, color):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.color = color
-    
-    def draw(self, window):
-        pygame.draw.circle(window, (0,0,0), (self.x,self.y), self.radius)
-        pygame.draw.circle(window, self.color, (self.x,self.y), self.radius-1)
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, vector):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_png("pinballsprite.png")
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.vector = vector
         
+    def update(self):
+        newpos = self.calcnewpos(self.rect,self.vector)
+        self.rect = newpos
 
-pinBall = ball(450, 550, 5, (255,255,255))
-run = True
-while run:
-    pygame.time.delay(80)
-    redrawWindow()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    def calcnewpos(self,rect,vector):
+        (angle,z) = vector
+        (dx,dy) = (z*math.cos(angle),z*math.sin(angle))
+        return rect.move(dx,dy)
+        
+def main():
+    pygame.init()
+    window = pygame.display.set_mode((500, 600))
+    ic= pygame.image.load("C:/Users/Pedro Silva/Documents/FEUP/FPRO/Projeto/pinball.jpg")
+    icon= pygame.display.set_icon(ic)
+    pygame.display.set_caption("PYnball")
+    
+    clock= pygame.time.Clock()
+    while 1:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return pygame.quit()
+            elif event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                if event.key == K_RIGHT:
+            
+    
+        
     
 
-pygame.quit()
+if __name__ == '__main__': main()
