2d19a4d8aca4c1666dd90b15e874d9bdbed9c428
pedrosilva17 <75742355+pedrosilva17@users.noreply.github.com>
2021-01-05 12:25:40 +0000

    Add files via upload

--- PYnball.py
+++ PYnball.py
@@ -14,6 +14,12 @@ import getopt
 from socket import *
 from pygame.locals import *
 
+#ecrã e janela
+
+ic= pygame.image.load("C:/Users/Pedro Silva/Documents/FEUP/FPRO/Projeto/pinball.jpg")
+icon= pygame.display.set_icon(ic)
+pygame.display.set_caption("PYnball")
+
 def load_png(name):
     fullname = os.path.join('C:/Users/Pedro Silva/Documents/FEUP/FPRO/Projeto', name)
     try:
@@ -27,53 +33,51 @@ def load_png(name):
         raise SystemExit(message)
     return image, image.get_rect()
 
-def redrawWindow():
-    window.fill((60,60,60))
-    pygame.display.update()
+screen = pygame.display.set_mode((500, 600))
 
-    
-x = 50
-y = 50
-width = 30
-height = 50
-vel = 5
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill((80, 80, 80))
+
+
 
 class Ball(pygame.sprite.Sprite):
-    def __init__(self, vector):
+    def __init__(self, x, y, speed, angle):
         pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_png("pinballsprite.png")
+        self.image, self.rect = load_png("rsz_pinballsprite.png")
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
-        self.vector = vector
+        self.speed = speed
+        self.angle = math.radians(-angle)
         
     def update(self):
-        newpos = self.calcnewpos(self.rect,self.vector)
-        self.rect = newpos
+        delta_x = self.speed*math.cos(self.angle)
+        delta_y = self.speed*math.sin(self.angle)
+        self.rect = self.rect.move(delta_x, delta_y)
+        if self.rect.right >= screen.get_width() or self.rect.left <= 0:
+            hit_bounds = True
+            self.angle = math.pi - self.angle
+        if self.rect.top <= 0 or self.rect.bottom >= screen.get_height():
+            hit_bounds = True
+            self.angle = -self.angle
 
-    def calcnewpos(self,rect,vector):
-        (angle,z) = vector
-        (dx,dy) = (z*math.cos(angle),z*math.sin(angle))
-        return rect.move(dx,dy)
         
 def main():
     pygame.init()
-    window = pygame.display.set_mode((500, 600))
-    ic= pygame.image.load("C:/Users/Pedro Silva/Documents/FEUP/FPRO/Projeto/pinball.jpg")
-    icon= pygame.display.set_icon(ic)
-    pygame.display.set_caption("PYnball")
-    
+    background.fill((80, 80, 80))
+    screen = pygame.display.set_mode((500, 600))
     clock= pygame.time.Clock()
+    speed = 9
+    ball = Ball(50, 50, 7, -45)
+    ballsprite = pygame.sprite.RenderPlain(ball)
     while 1:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == QUIT:
                 return pygame.quit()
-            elif event.type == KEYDOWN:
-                if event.key == K_LEFT:
-                if event.key == K_RIGHT:
-            
-    
-        
-    
+        screen.blit(background, (0,0))
+        ballsprite.update()
+        ballsprite.draw(screen)
+        pygame.display.flip()
 
 if __name__ == '__main__': main()
