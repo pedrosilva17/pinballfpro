51864fc2369c722cdd5208b97afce280a8ceafd3
pedrosilva17 <75742355+pedrosilva17@users.noreply.github.com>
2021-01-13 19:43:10 +0000

    Add files via upload

--- BouncyPYnball.py
+++ BouncyPYnball.py
@@ -6,12 +6,12 @@ Created on Sun Dec 13 13:49:25 2020
 """
 
 """
-BUGS:
+Observações:
     -Bola passa pelos suportes laterais dos flippers em certas
     situações, penso que seja em caso de incidência
     perpendicular à superfície (rever colisões)
-    -Bouncers prendem muito a bola, colisão não é suave (rever colisões)
-
+    
+    -Bouncers e flippers prendem muito a bola, colisão não é suave (rever colisões)
 """
     
 import pygame
@@ -24,12 +24,12 @@ from pygame.locals import *
 
 #ecrã e janela
 
-ic= pygame.image.load("C:/Users/Pedro Silva/Documents/FEUP/FPRO/Projeto/pinball.jpg")
+ic= pygame.image.load("C:/Users/Public/Documents/pinball.jpg")
 icon= pygame.display.set_icon(ic)
 pygame.display.set_caption("Bouncy PYnball")
 
 def load_png(name):
-    fullname = os.path.join('C:/Users/Pedro Silva/Documents/FEUP/FPRO/Projeto', name)
+    fullname = os.path.join('C:/Users/Public/Documents/', name)
     try:
         image = pygame.image.load(fullname)
         if image.get_alpha is None:
@@ -42,15 +42,17 @@ def load_png(name):
     return image, image.get_rect()
 
 screen = pygame.display.set_mode((500, 600))
-backtext = pygame.image.load("C:/Users/Pedro Silva/Documents/FEUP/FPRO/Projeto/backgroundtexture.png").convert()
+backtext = pygame.image.load("C:/Users/Public/Documents/backgroundtexture.png").convert()
 
 #cenas temporárias
+#esq: dx=130, dy=27, x=0, y=450
+#dir: dx=130, dy=27, x=370, y=450
 pygame.draw.rect(backtext, (255,0,0), (0,590,500,10))
-botleft = pygame.draw.polygon(backtext, (0,0,0), [(0,450),(150,500),(140,510),(0,460)])
-botright= pygame.draw.polygon(backtext, (0,0,0), [(500,450),(350,500),(360,510),(500,460)])
 pygame.draw.rect(backtext, (139,69,19), (0,0,10,600))
 pygame.draw.rect(backtext, (139,69,19), (490,0,10,600))
 pygame.draw.rect(backtext, (139,69,19), (0,0,500,10))
+support1 = pygame.draw.rect(backtext, (139,69,19), (10,480,112,120))
+support2 = pygame.draw.rect(backtext, (139,69,19), (383,480,112,120))
 #fim das cenas temporárias
 
 class Ball(pygame.sprite.Sprite):
@@ -58,10 +60,13 @@ class Ball(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_png("rsz_pinballsprite.png")
         screen = pygame.display.get_surface()
+        surf = pygame.Surface((6,7))
+        self.surf = surf
         self.area = screen.get_rect()
         self.speed = speed
         self.angle = math.radians(-angle)
         self.radius = 6
+        self.mask = pygame.mask.from_surface(surf)
         
     def update(self):
         delta_x = self.speed*math.cos(self.angle)
@@ -73,32 +78,55 @@ class Ball(pygame.sprite.Sprite):
             self.angle = math.pi - self.angle
         if self.rect.top <= 10:
             self.angle = -self.angle
-        for x,y in zip(range(0,150,3),range(450,500,1)):
-            if y+15 > self.rect.bottom > y and x-5 < self.rect.left < x:
-                self.angle = math.radians(30) - self.angle
-            #(debug colisão) pygame.draw.rect(backtext, (0,0,0), (x,y,5,20))
-        for x,y in zip(range(500,350,-3),range(450,500,1)):
-            if y+15 > self.rect.bottom > y and x-5 < self.rect.right < x:
-                self.angle = -math.radians(30) - self.angle    
-            #(debug colisão) pygame.draw.rect(backtext, (0,0,0), (x,y,5,20))
 
 class Bouncer(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_png("bumper1.png")
+        surf = pygame.Surface((58,58))
+        self.surf = surf
         screen = pygame.display.get_surface()
         self.radius = 29
+        self.mask = pygame.mask.from_surface(surf)
 
+class LeftFlipper(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_png("flipper1.png")
+        screen = pygame.display.get_surface()
+        surf = pygame.Surface((94,27))
+        self.surf = surf
+        self.area = screen.get_rect()
+        self.mask = pygame.mask.from_surface(surf)
+        
+        
+class RightFlipper(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_png("flipper2.png")
+        screen = pygame.display.get_surface()
+        surf = pygame.Surface((94,27))
+        self.surf = surf
+        self.area = screen.get_rect()
+        self.mask = pygame.mask.from_surface(surf)
 
         
 def main():
+#JANELA
     pygame.init()
     screen = pygame.display.set_mode((500, 600))
+    
+#TEXTO
     title = pygame.font.SysFont("Comic Sans MS", 38, True)
     subtitle = pygame.font.SysFont("Comic Sans MS", 20)
     titlesurf = title.render("Bouncy PYnball", True, (255,255,255))
     subtitlesurf = subtitle.render("Press 'Spacebar' to begin!", True, (255,255,255))
+    
+#CLOCK
     clock = pygame.time.Clock()
+    
+#OBJETOS
+    keys = pygame.key.get_pressed()
     ball = Ball(0, random.randint(210,330))
     ball.rect.x=250
     ball.rect.y=30
@@ -109,33 +137,100 @@ def main():
     bouncer1sprite = pygame.sprite.RenderPlain(bouncer1)
     bouncer2sprite = pygame.sprite.RenderPlain(bouncer2)
     bouncer3sprite = pygame.sprite.RenderPlain(bouncer3)
+    (flipper1, flipper2) = (LeftFlipper(), RightFlipper())
+    flipper1.rect.x, flipper2.rect.x = (120, 290)
+    flipper1.rect.y, flipper2.rect.y = (478, 478)
+    flipper1sprite = pygame.sprite.RenderPlain(flipper1)
+    flipper1ogimage = flipper1.image
+    flipper1ogsurf = flipper1.surf
+    flipper2ogimage = flipper2.image
+    flipper2ogsurf = flipper2.surf
+    flipper2sprite = pygame.sprite.RenderPlain(flipper2)
+    
+#WHILE LOOP
     while True:
-        distance1 = math.hypot(ball.rect.centerx - bouncer1.rect.centerx, ball.rect.centery - bouncer1.rect.centery)
-        distance2 = math.hypot(ball.rect.centerx - bouncer2.rect.centerx, ball.rect.centery - bouncer2.rect.centery)
-        distance3 = math.hypot(ball.rect.centerx - bouncer3.rect.centerx, ball.rect.centery - bouncer3.rect.centery)
         tangent1 = math.atan2(ball.rect.centery-bouncer1.rect.centery, ball.rect.centerx-bouncer1.rect.centerx)
         tangent2 = math.atan2(ball.rect.centery-bouncer2.rect.centery, ball.rect.centerx-bouncer2.rect.centerx)
         tangent3 = math.atan2(ball.rect.centery-bouncer3.rect.centery, ball.rect.centerx-bouncer3.rect.centerx)
+        colangle1 = 0.5 * math.pi + tangent1
+        colangle2 = 0.5 * math.pi + tangent2
+        colangle3 = 0.5 * math.pi + tangent3 
         for event in pygame.event.get():
             if event.type == QUIT:
                 return pygame.quit()
-            if event.type==pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    ball.speed = 6
+                    ball.speed = 9
                     titlesurf = pygame.Surface((0,0))
                     subtitlesurf = pygame.Surface((0,0))
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    flipper1.image= pygame.transform.rotate(flipper1.image, 30)
+                    flipper1.surf= pygame.transform.rotate(flipper1.surf, 30)
+                    flipper1.mask= pygame.mask.from_surface(flipper1.surf)
+                    flipper1.rect.x = 110
+                    flipper1.rect.y = 430
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    flipper2.image= pygame.transform.rotate(flipper2.image, -30)
+                    flipper2.surf= pygame.transform.rotate(flipper2.surf, -30)
+                    flipper2.mask= pygame.mask.from_surface(flipper2.surf)
+                    flipper2.rect.x = 300
+                    flipper2.rect.y = 430
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    flipper1.image= flipper1ogimage
+                    flipper1.surf= flipper1ogsurf
+                    flipper1.mask= pygame.mask.from_surface(flipper1.surf)
+                    flipper1.rect.x = 120
+                    flipper1.rect.y = 478
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    flipper2.image= flipper2ogimage
+                    flipper2.surf= flipper2ogsurf
+                    flipper2.mask= pygame.mask.from_surface(flipper2.surf)
+                    flipper2.rect.x = 290
+                    flipper2.rect.y = 478
+                if event.key == pygame.K_k:
+                    ball.angle+= math.radians(30)
+#COLISÕES
         if ball.rect.bottom >= 590:
             ball.remove()
             ball = Ball(0, random.randint(210,330))
             ballsprite = pygame.sprite.RenderPlain(ball)
             ball.rect.x=250
-            ball.rect.y=30
-        if ball.radius + bouncer1.radius >= distance1:
-            ball.angle = 2 * tangent1 - ball.angle
-        if ball.radius + bouncer2.radius >= distance2:
+            ball.rect.y=30    
+        if pygame.sprite.collide_mask(ball, bouncer1):
+                ball.angle = 2 * tangent1 - ball.angle
+                ball.rect.x += 2 * math.sin(colangle1)
+                ball.rect.y -= 2 * math.cos(colangle1)
+        if pygame.sprite.collide_mask(ball, bouncer2):
             ball.angle = 2 * tangent2 - ball.angle
-        if ball.radius + bouncer3.radius >= distance3:
+            ball.rect.x += 2 * math.sin(colangle2)
+            ball.rect.y -= 2 * math.cos(colangle2)
+        if pygame.sprite.collide_mask(ball, bouncer3):
             ball.angle = 2 * tangent3 - ball.angle
+            ball.rect.x += 2 * math.sin(colangle3)
+            ball.rect.y -= 2 * math.cos(colangle3)
+        if pygame.sprite.collide_mask(ball, flipper1):
+            if flipper1.surf == flipper1ogsurf:
+                ball.angle = math.radians(30) - ball.angle
+                ball.rect.x += 2
+                ball.rect.y -= 3
+            else:
+                ball.angle = -math.radians(30) - ball.angle
+                ball.rect.x -= 2
+                ball.rect.y -= 3
+        if pygame.sprite.collide_mask(ball, flipper2):
+            if flipper2.surf == flipper2ogsurf:
+                ball.angle = -math.radians(30) - ball.angle
+                ball.rect.x -= 2
+                ball.rect.y -= 3
+            else:
+                ball.angle = math.radians(30) - ball.angle
+                ball.rect.x += 2
+                ball.rect.y -= 3
+        if ball.rect.colliderect(support1) or ball.rect.colliderect(support2):
+            ball.angle = -ball.angle
+
+#UPDATES E BLITS
         clock.tick(60)
         screen.blit(backtext, (0,0))
         screen.blit(titlesurf, (120,100))
@@ -145,11 +240,14 @@ def main():
         bouncer1sprite.draw(screen)
         bouncer2sprite.draw(screen)
         bouncer3sprite.draw(screen)
+        flipper1sprite.draw(screen)
+        flipper2sprite.draw(screen)
         pygame.display.flip()
 
-ball = Ball(0, random.randint(220,310))
-ball.rect.x=250
-ball.rect.y=30
+#ball = Ball(0, random.randint(220,310))
+#ball.rect.x=250
+#ball.rect.y=30
+
 #print(ball.rect.center)
 #centro da bola: (6,7) em relação ao c.sup.esq
 #raio: 6
@@ -163,5 +261,6 @@ ball.rect.y=30
 
 #distance = math.hypot(ball.rect.centerx - bouncer.rect.centerx, ball.rect.centery - bouncer.rect.centery)
 #print(distance)
+#(flipper1, flipper2) = (LeftFlipper(), RightFlipper())
 
 if __name__ == '__main__': main()
